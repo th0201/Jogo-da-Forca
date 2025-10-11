1387ff376053eb6980967eedf3baeb5971a86753
MariaLuiza98 <142938147+MariaLuiza98@users.noreply.github.com>
2025-10-10 21:11:38 -0300

    Update TelaInicial.java

--- TelaInicial.java
+++ TelaInicial.java
@@ -22,47 +22,4 @@ public class TelaInicial extends JFrame {
         JPanel painelBoneco = new JPanel(new GridBagLayout());
         painelBoneco.setBackground(Color.WHITE);
 
-        JTextArea boneco = new JTextArea();
-        boneco.setEditable(false);
-        boneco.setFont(new Font("Monospaced", Font.PLAIN, 18));
-        boneco.setBackground(Color.WHITE);
-        boneco.setText(
-                "   +---+\n" +
-                "   |   |\n" +
-                "   O   |\n" +
-                "  /|\\  |\n" +
-                "  / \\  |\n" +
-                "       |\n" +
-                "========="
-        );
-        painelBoneco.add(boneco);
-        add(painelBoneco, BorderLayout.CENTER);
-
         
-        JButton jogarBtn = new JButton("Jogar");
-        jogarBtn.setFont(new Font("Arial", Font.BOLD, 22));
-        jogarBtn.setBackground(new Color(0, 102, 204));
-        jogarBtn.setForeground(Color.WHITE);
-        jogarBtn.setFocusPainted(false);
-        jogarBtn.setCursor(new Cursor(Cursor.HAND_CURSOR));
-        jogarBtn.setPreferredSize(new Dimension(150, 50));
-        jogarBtn.setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20));
-        jogarBtn.setBorder(BorderFactory.createLineBorder(new Color(0, 80, 160), 2, true));
-
-        jogarBtn.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                dispose();
-                new InterfaceGUI();
-            }
-        });
-
-        JPanel painelBotao = new JPanel();
-        painelBotao.setBackground(Color.WHITE);
-        painelBotao.add(jogarBtn);
-
-        add(painelBotao, BorderLayout.SOUTH);
-
-        setVisible(true);
-    }
-}
